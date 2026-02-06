cb8e5c1e036b87ba2135b402e22a04817baebfe8
Anton Syzov <aantonssizov@gmail.com>
2026-02-06 17:49:16 +0100

    favicon, logo, colors, fonts

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
-import { defineConfig } from 'vite'
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from 'vite';
 
-export default defineConfig({})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+    root: "src/",
+
+    build: {
+        outDir: "../dist",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+            },
+        },
+    }
+})
